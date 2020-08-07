1c80d49acb5bc08eb47e3254594276e05602c9d5
Andrew <afmcdnl@gmail.com>
2020-08-07 15:21:29 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+let living = ["Mansion", "Apartment", "Shack", "House"]
+let Partner = ["Mansion", "Apartment", "Shack", "House"]
+let Kids = [1, 2, 3, 4]
+let Cars = ["a", "b", "c"]
+let Number2 = randint(2, 8)
+basic.forever(function () {
+	
+})
